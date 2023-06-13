e59fe73051788f2818581b5e334c269cd220a993
AnaJRe <130793343+AnaJRe@users.noreply.github.com>
2023-06-13 17:00:39 -0500

    Add files via upload
    Se hicieron mejoras a los códigos para mejorar responsive

--- script.js
+++ script.js
@@ -1,64 +1,76 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
-const copia = document.querySelector(".copiar");
+const copia = document.querySelector(".copiar")
 
+const mensajeInput = document.querySelector(".mensaje");
+const copiarButton = document.querySelector(".btn-copiar");
+const encriptarButton = document.querySelector(".btn-encriptar");
 
+encriptarButton.addEventListener('click', function() {
+  if (mensajeInput.value.trim().length > 0) {
+    copiarButton.style.display = "inline-block";
+  } else {
+    copiarButton.style.display = "none";
+  }
+});
 
-function ValidarTxt() {
-    let txtEscrito = document.querySelector(".text-area").value;
-    let validador = txtEscrito.match(/^[a-z]*$/);
-    if(!validador || validador === 0) {
-        alert("Solo son permitidas letras minúsculas y sin acentos");
-        location.reload();
+function validarTxt() {
+    let txtEscrito = textArea.value;
+    let validador = txtEscrito.match(/^[a-z\s]*$/);
+    if (!validador || validador === 0) {
+        alert("Solo se permiten letras minúsculas y sin acentos");
         return true;
     }
+    return false;
 }
 
-function btnEncriptar(){
-    if(!ValidarTxt()) {
-        const txtEncriptado = encriptar(textArea.value)
+function btnEncriptar() {
+    if (!validarTxt()) {
+        const txtEncriptado = encriptar(textArea.value);
         mensaje.value = txtEncriptado;
         mensaje.style.backgroundImage = "none";
         textArea.value = "";
-        copia.style.display = "block"
     }
 }
-//Laves de encriptacion
+
+// Laves de encriptacion
 // `La letra "e" es convertida para "enter"`
 // `La letra "i" es convertida para "imes"`
 // `La letra "a" es convertida para "ai"`
 // `La letra "o" es convertida para "ober"`
 // `La letra "u" es convertida para "ufat"`
 
-
-function encriptar(stringEncriptada){
+function encriptar(stringEncriptada) {
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase()
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    stringEncriptada = stringEncriptada.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replace(new RegExp(matrizCodigo[i][0], "g"), matrizCodigo[i][1]);
         }
     }
-    return stringEncriptada
+    return stringEncriptada;
 }
-function btnDesencriptar(){
-    const textoEncriptado = desencriptar(textArea.value)
-    mensaje.value = textoEncriptado
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
     textArea.value = "";
 }
-function desencriptar(stringDesencriptada){
+
+function desencriptar(stringDesencriptada) {
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase()
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringDesencriptada.includes(matrizCodigo[i][1])){
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] , matrizCodigo[i][0])
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replace(new RegExp(matrizCodigo[i][1], "g"), matrizCodigo[i][0]);
         }
     }
-    return stringDesencriptada
+    return stringDesencriptada;
 }
-function copiar(){
+
+function copiar() {
     mensaje.select();
-    navigator.clipboard.writeText(mensaje.value)
+    navigator.clipboard.writeText(mensaje.value);
     mensaje.value = "";
-    alert("Texto Copiado")
+    alert("Texto Copiado");
 }
